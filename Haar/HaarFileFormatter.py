45b037d1f4774aba9535ff42d8c07d2c12038bd3
si31 <s.j.houghton@hotmail.com>
2018-01-08 09:13:11 +0000

    Name changes on Haar resources

--- Haar/HaarFileFormatter.py
+++ Haar/HaarFileFormatter.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 
 def main():
 	i = 0
@@ -9,11 +10,15 @@ def main():
 		i += 1
 
 def main2():
-	with open('Pos/bg.txt', 'w+') as f:
-		for i in range(0, 51):
-			f.write("img/img" + str(i) + ".jpg\r\n")
+	with open('Haar/info.dat.txt', 'w+') as f:
+		path = "/Users/admin/Documents/AudienceAttention/Haar/Pos/"
+		for filename in os.listdir(path):
+			print(filename)
+			if filename.startswith("img") and filename != "img3.jpg":
+				img = cv2.imread(path+filename)
+				f.write("Pos/" + filename + " 1 0 0 " + str(img.shape[0]) + " " + str(img.shape[1]) + "\r\n")
 
 
 
 if __name__ == "__main__":
-	main()
+	main2()
