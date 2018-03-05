b7370e6950aef2e8d0f4de3930c96d69fb8c1b2b
si31 <s.j.houghton@hotmail.com>
2018-03-05 17:30:16 +0000

    started work on tracking bbes in videos

--- Video.py
+++ Video.py
@@ -5,4 +5,17 @@ class Video:
 
 	def __init__(self):
 		self.frames = []
-		self.frameObjects = []
+		self.persons = [] #array of videPerson objects
+
+	def trackPersonsAcrossFrames(self):
+		for index in range(len(frames)-1):
+			trackPersonsAcrossFrames(frames[i], frames[i+1])
+
+	def trackPersonsAcrossTwoFrames(frame1, frame2):
+
+class VideoPerson:
+
+	def __init__(self):
+		self.imagePersons = [] # length of no. of frames, each points to the person in the frame or None if no person detected
+		self.averagePosition = (0,0,0,0)
+		self.averageAttention = 0
