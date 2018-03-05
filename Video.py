6bffbb109c713eab8c527da5b3674875df629603
si31 <s.j.houghton@hotmail.com>
2018-03-05 19:41:12 +0000

    finished but not tested

--- Video.py
+++ Video.py
@@ -5,17 +5,46 @@ class Video:
 
 	def __init__(self):
 		self.frames = []
-		self.persons = [] #array of videPerson objects
+		self.persons = [] #array of videoPerson objects
 
-	def trackPersonsAcrossFrames(self):
-		for index in range(len(frames)-1):
-			trackPersonsAcrossFrames(frames[i], frames[i+1])
+	def trackPersonAcrossFrames(self):
+		for person in frames[0].persons:
+			videoPerson = VideoPerson(person)
+			self.persons.append(videoPerson, 0)
+		for index, frame in enumerate(self.frames[1:]):
+			for personInFrame in frame2.persons:
+				trackPersonsAcrossFrames(index, personInFrame, nextFrame)
 
-	def trackPersonsAcrossTwoFrames(frame1, frame2):
+	def trackPersonAcrossTwoFrames(index, personInFrame, nextFrame): #each are Image objects
+		found = False
+		for videoPerson in self.persons:
+			if HelperFunctions.bbOverLapRatio(personInFrame.face, videoPerson.averagePosition) > 0.1:
+				found = True
+				videoPerson.addFrame(personInFrame, index)
+				break
+		if not found:
+			self.persons.append(VideoPerson(personInFrame, index))
+
+	def fillInFrameHoles():
+		pass # fill in frames that do not have a person in from not being detected
+
 
 class VideoPerson:
 
-	def __init__(self):
-		self.imagePersons = [] # length of no. of frames, each points to the person in the frame or None if no person detected
+	def __init__(self, initalFramePerson, initalFrameIndex):
+		self.imagePersons = [None] * len(self.frames) # length of no. of frames, each points to the person in the frame or None if no person detected
+		self.imagePersons[initalFrameIndex] = initalFramePerson 
 		self.averagePosition = (0,0,0,0)
 		self.averageAttention = 0
+		self.numberOfFramesIn = 1
+
+	def addFrame(self, personInFrame, frameIndex):
+		self.imagePersons[frameIndex] = personInFrame
+		self.numberOfFramesIn += 1
+		self.averagePosition = self.newAveragePosition(personInFrame.face)
+
+	def newAveragePosition(self, newPosition):
+		(x1,y1,w1,h1) = self.averagePosition
+		(x2,y2,w2,h2) = self.face
+		self.averagePosition = ((x1+x2)//2,(y1+y2)//2,(w1+w2)//2,(h1+h2)//2)
+
